c6622e6b6d77104cabf09d19d6773a9541035c98
timothychen93 <timothy.c@borngroup.com>
2019-04-09 22:11:16 -0400

    tryIgnore

---
+++ app/etc/env.php
@@ -0,0 +1,64 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'adminlogin'
+    ],
+    'crypt' => [
+        'key' => '65b07bf53f4d7ee8f4b884c102e5ee62'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2',
+                'username' => 'magento_user',
+                'password' => 'mi@$wEw*d8',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '5a4_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '5a4_'
+            ]
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'install' => [
+        'date' => 'Tue, 09 Apr 2019 15:15:44 +0000'
+    ]
+];
